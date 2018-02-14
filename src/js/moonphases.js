f5b07f52bca2e15360adbca3fc639369370154e9
Binh Nguyen <b.nguyen.sn@gmail.com>
2018-02-14 01:00:13 +0000

    Update the getNMSandwichDates() function which should work more properly now

--- src/js/moonphases.js
+++ src/js/moonphases.js
@@ -2,27 +2,63 @@
 
 const MS_IN_DAY = 86400000;
 
-const MEAN_LUNATION_DAY = 29.5295;  // Mean value of a moon synodic month
-const MEAN_LUNATION_MS = MEAN_LUNATION_DAY * MS_IN_DAY;
+const MEAN_LUNATION_DAYS = 29.530588;  // Mean value of a moon synodic month
+const MEAN_LUNATION_MS = MEAN_LUNATION_DAYS * MS_IN_DAY;
 
-const NEW_MOON_EPOCH_DATE = new Date(2000, 0, 1, 12, 24, 1);  // We know there's a New Moon on this date
+const NEW_MOON_EPOCH_DATE = new Date(2000, 0, 6, 18, 14);  // There was a New Moon starting on this Date
 const NEW_MOON_EPOCH_MS = NEW_MOON_EPOCH_DATE.getTime();
 
 /**
- * Return the approximate closest New Moon date to a given date
+ * Return the approximate New Moon dates sandwiching a given date
  * @param {Date} date A given date
- * @return {Date} nm_date Closest New Moon date
+ * @return {Array} [NM_date1, NM_date2] New Moon dates sandwiching the given date. If the given date is a New Moon Date, NM_date 2 === undefined.
  * */
-function getClosestNM(date) {
+function getNMSandwichDates(date) {
     const date_ms = date.getTime();
+    const date_norm = date.setHours(0, 0, 0, 0);  // Normalise to 00:00:00:0000
 
     // Find out the ms difference between the New Moon epoch and our given date
-    // Note that date.getTime() returns a ms value relative to the 1970 epoch, which means it could be < 0
-    let date_elapsed_ms = 0;
-    if (date_ms < 0) {
-        date_elapsed_ms = -date_ms + NEW_MOON_EPOCH_MS;
-    } else {
-        date_elapsed_ms = Math.abs(date_ms - NEW_MOON_EPOCH_MS);
+    const date_elapsed_ms = date_ms - NEW_MOON_EPOCH_MS;
+
+    // Find the two New Moon dates sandwiching our given date
+    const low_end_LP = Math.floor(date_elapsed_ms / MEAN_LUNATION_MS);
+    const low_end_NMDate = new Date(NEW_MOON_EPOCH_MS + low_end_LP * MEAN_LUNATION_MS);
+    const low_end_NMDate_norm = new Date(low_end_NMDate);
+    low_end_NMDate_norm.setHours(0, 0, 0, 0);
+
+    let high_end_LP;
+    let high_end_NMDate;
+    let high_end_NMDate_norm;
+
+    if (date_norm !== low_end_NMDate_norm) {
+        high_end_LP = low_end_LP + 1;
+        high_end_NMDate = new Date(NEW_MOON_EPOCH_MS + high_end_LP * MEAN_LUNATION_MS);
+        high_end_NMDate_norm = new Date(high_end_NMDate);
+        high_end_NMDate_norm.setHours(0, 0, 0, 0);
     }
+    
+    return [low_end_NMDate, high_end_NMDate]
+}
+
+/**
+ * 
+ * */
+function getChineseNewYear(year) {
 
 }
+
+function test() {
+    const d = new Date(2028, 1, 14);
+
+    const a = getNMSandwichDates(d);
+
+    console.log(`NMDate 1 = ${a[0]}\nNMDate 2 = ${a[1]}`);
+}
+
+test();
+
+/*
+module.exports = {
+    getNMSandwichDates: getNMSandwichDates
+};
+*/
