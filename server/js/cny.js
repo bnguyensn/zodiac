3b592f552de0b13091ae141e06fdb3617ac7901e
Binh Nguyen <b.nguyen.sn@gmail.com>
2018-02-28 23:50:08 +0000

    Slight update to the server's createCNYlist.js

--- server/js/cny.js
+++ server/js/cny.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import * as moonphases from './moonphases';
+const moonphases = require('./moonphases');
 
 /* ********** CONSTANTS ********** */
 
--- server/js/createCNYlist.js
+++ server/js/createCNYlist.js
@@ -3,8 +3,39 @@
 const fs = require('fs');
 const path = require('path');
 
-function createCNYlist() {
+const cny = require('./cny');
+
+/**
+ * Check if the passed parameter is a valid UTC year
+ * @param n Anything
+ * @return {Boolean} Return true if the passed parameter is a valid UTC year
+ * */
+function validateYear(n) {
     
+}
+
+/**
+ * Return an object containing the Chinese New Year dates for each given UTC year
+ * @param {int} start A UTC year. If providing a range of years, this will be the start of the range
+ * @param {int} stop Default to the start year. If providing a range of years, this will be the end of the range
+ * @param {int} step Default to 1. If providing a range of years, this will be the step amount
+ * @return {Object} An object containing the Chinese New Year dates for each given UTC year
+ * */
+function createCNYlist(start, stop = start, step = 1) {
+    if (!validateYear(start) || !validateYear(stop) || !Number.isInteger(step)) {
+        throw Error
+    }
+
+    if (stop === start) {
+        // Only 1 year was provided
+        return {[start]: cny.getCNY(start)}
+    } else if (stop < start) {
+        // TODO:
+
+    } else {
+        // TODO:
+
+    }
 }
 
 function writeCNYlist(data) {
